cddafaec7bd043be3df870a40a43077936469ccf
malladibhanu12 <134955553+malladibhanu12@users.noreply.github.com>
2023-06-06 19:07:51 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Loading,please wait");
+var data1={image : "bhanu.jpg",name : " BHANU PRAKASH", designation:"Software Engineer"};
+var data2={image : "m.bhanu.jpg",name : "MALLADI BHANU", designation:"Software Developer"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata= data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("myname").innerHTML=displaydata.name;
+    document.getElementById("mydesig").innerHTML=displaydata.designation;
+}
