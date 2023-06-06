b96ea2d14859e46509bf191a7bb7d44aea22a973
malladibhanu12 <134955553+malladibhanu12@users.noreply.github.com>
2023-06-06 19:19:48 +0530

    image added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var data1={image : "bhanu.jpg",name : " BHANU PRAKASH", designation:"Software Engineer"};
-var data2={image : "m.bhanu.jpg",name : "MALLADI BHANU", designation:"Software Developer"};
+var data1={image : "./bhanu.jpg",name : " BHANU PRAKASH", designation:"Software Engineer"};
+var data2={image : "./m.bhanu.jpg",name : "MALLADI BHANU", designation:"Software Developer"};
 var isdata1 = true;
 var displaydata;
 var changemypic = function(){
